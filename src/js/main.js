449689f3776c60596b183a64cc290265ee3c13dc
ftorrelles <torrellesf93@gmai.com>
2022-11-04 00:41:26 -0400

    modificar el nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,16 @@
-const iconMenu = document.querySelector(".menu_ham");
+const iconMenu = document.querySelector(".bx-menu");
 const menu = document.querySelector(".menuNav");
 
 iconMenu.addEventListener("click", function () {
+    iconMenu.classList.toggle("bx-x");
     menu.classList.toggle("menuNav_show");
+});
+
+const cerrar = document.querySelectorAll('.menuNav a[href^="#"]');
+
+cerrar.forEach((cerrar) => {
+    cerrar.addEventListener("click", function () {
+        iconMenu.classList.remove("bx-x");
+        menu.classList.remove("menuNav_show");
+    });
 });
